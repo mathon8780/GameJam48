38dee61f5c944e65de9834c2eddf5a502c5aa561
mathon8780 <mathon.8780@outlook.com>
2025-09-19 21:44:25 +0800

    fixed data config

--- Assets/Scripts/DataConfig/PlayerAttributes.cs
+++ Assets/Scripts/DataConfig/PlayerAttributes.cs
@@ -5,7 +5,8 @@ namespace DataConfig
     [CreateAssetMenu(fileName = "New PlayerAttribute", menuName = "Player Attribute", order = 0)]
     public class PlayerAttributes : ScriptableObject
     {
-        [Tooltip("血量")] public float Hp;
+        [Tooltip("基础血量")] public float Hp;
+        [Tooltip("最大血量")] public float MaxHp;
         [Tooltip("修正血量")] public float FixedHp;
         [Tooltip("移动速度")] public float MoveSpeed;
         [Tooltip("跳跃高度")] public float JumpHeight;
