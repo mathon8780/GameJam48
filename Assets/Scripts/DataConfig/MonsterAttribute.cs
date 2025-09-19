de1843cf6f5f335328cbd76e8e055f409e73810b
mathon8780 <mathon.8780@outlook.com>
2025-09-19 21:33:47 +0800

    Data Config

---
+++ Assets/Scripts/DataConfig/MonsterAttribute.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace DataConfig
+{
+    [CreateAssetMenu(fileName = "New MonsterAttribute", menuName = "MonsterAttribute", order = 0)]
+    public class MonsterAttribute : ScriptableObject
+    {
+        [Tooltip("初始素质值")] public float StartQuality;
+        [Tooltip("阈值A")] public float ThresholdA;
+        [Tooltip("变换范围A")] public float ThresholdAChangeRange;
+        [Tooltip("阈值B")] public float ThresholdB;
+        [Tooltip("变换范围B")] public float ThresholdBChangeRange;
+        [Tooltip("最大错误次数")] public int MaxMistakes;
+        [Tooltip("攻击力")] public float Atk;
+        [Tooltip("愤怒值")] public float Rage;
+        [Tooltip("冷静阈值")] public float CalmnessThreshold;
+    }
+}
---
+++ Assets/Scripts/DataConfig/PlayerAttributes.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace DataConfig
+{
+    [CreateAssetMenu(fileName = "New PlayerAttribute", menuName = "Player Attribute", order = 0)]
+    public class PlayerAttributes : ScriptableObject
+    {
+        [Tooltip("血量")] public float Hp;
+        [Tooltip("移动速度")] public float MoveSpeed;
+        [Tooltip("跳跃高度")] public float JumpHeight;
+        [Tooltip("防御力")] public float Defense;
+        [Tooltip("冷静力")] public float Calmness;
+    }
+}
