abd9161731ccacaef43d10ad5e5aa06fbe592c06
mathon8780 <mathon.8780@outlook.com>
2025-09-19 23:04:22 +0800

    fixed wrong

--- Assets/Scripts/Character/RedColorMonsterController.cs
+++ Assets/Scripts/Character/RedColorMonsterController.cs
@@ -1,22 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<<< HEAD:Assets/Scripts/Character/RedColorMonsterController.cs
-public class RedColorMonster : MonoBehaviour
-========
-public class HpInfo : MonoBehaviour
->>>>>>>> d5a842a1918c955df4801d2d1bd11baf3bff2d57:Assets/Scripts/UI/HpInfo.cs
+namespace Character
 {
-    // Start is called before the first frame update
-    void Start()
+    public class RedColorMonster : MonoBehaviour
     {
-        
-    }
+        public class HpInfo : MonoBehaviour
+        {
+            // Start is called before the first frame update
+            void Start()
+            {
+            }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            // Update is called once per frame
+            void Update()
+            {
+            }
+        }
     }
 }
