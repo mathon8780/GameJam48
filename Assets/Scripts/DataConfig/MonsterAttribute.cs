54a9d39826510374bb55eda303a367afc13406e9
mathon8780 <mathon.8780@outlook.com>
2025-09-19 21:41:15 +0800

    Fixed Data Config

--- Assets/Scripts/DataConfig/MonsterAttribute.cs
+++ Assets/Scripts/DataConfig/MonsterAttribute.cs
@@ -6,13 +6,21 @@ namespace DataConfig
     public class MonsterAttribute : ScriptableObject
     {
         [Tooltip("初始素质值")] public float StartQuality;
+        [Tooltip("修正素质值")] public float FixedQuality = 0;
+
         [Tooltip("阈值A")] public float ThresholdA;
         [Tooltip("变换范围A")] public float ThresholdAChangeRange;
+        [Tooltip("修正阈值A 默认0")] public float FixedThresholdA = 0;
+
         [Tooltip("阈值B")] public float ThresholdB;
         [Tooltip("变换范围B")] public float ThresholdBChangeRange;
+        [Tooltip("修正阈值B 默认0")] public float FixedThresholdB = 0;
+
         [Tooltip("最大错误次数")] public int MaxMistakes;
+        [Tooltip("当前错误次数 默认0")] public int CurrentMistakes = 0;
+
         [Tooltip("攻击力")] public float Atk;
-        [Tooltip("愤怒值")] public float Rage;
-        [Tooltip("冷静阈值")] public float CalmnessThreshold;
+        [Tooltip("愤怒值/红温度")] public float Rage;
+        [Tooltip("冷静阈值 默认40")] public float CalmnessThreshold = 40;
     }
 }
--- Assets/Scripts/DataConfig/PlayerAttributes.cs
+++ Assets/Scripts/DataConfig/PlayerAttributes.cs
@@ -6,9 +6,10 @@ namespace DataConfig
     public class PlayerAttributes : ScriptableObject
     {
         [Tooltip("血量")] public float Hp;
+        [Tooltip("修正血量")] public float FixedHp;
         [Tooltip("移动速度")] public float MoveSpeed;
         [Tooltip("跳跃高度")] public float JumpHeight;
-        [Tooltip("防御力")] public float Defense;
         [Tooltip("冷静力")] public float Calmness;
+        [Tooltip("防御力")] public float Defense;
     }
 }
